932419b53389ad5640ef79ca9ca144c2c0ed1912
Caryn Ligon <carynligon@gmail.com>
2017-01-30 22:05:33 -0600

    timer rendering correctly

--- src/app/Components/timer.js
+++ src/app/Components/timer.js
@@ -10,20 +10,39 @@ import TimerMixin from 'react-timer-mixin';
 export class Timer extends Component {
     constructor(props) {
         super(props);
-        this.state = {number: 0};
+        this.state = {
+            minutes: 0,
+            seconds: 0,
+            timerTextMins: '00',
+            timerTextSecs: '00'
+        };
         this.timer;
     }
 
     updateSomething() {
-        this.setState({number: this.state.number + 1});
-        this.timer = setTimeout(this.updateSomething.bind(this), 1000);
-    }
+        this.setState({totalSeconds: (this.state.minutes * 60 + this.state.seconds)});
+        if (this.state.seconds < 59) {
+            this.setState({seconds: this.state.seconds + 1})
+            if (this.state.seconds <= 9 && this.state.minutes <= 9) {
+               this.setState({timerText: '0' + this.state.minutes + ':' + '0' + this.state.seconds})
+             } else if (this.state.seconds >= 10 && this.state.minutes <= 9) {
+               this.setState({timerText: '0' + this.state.minutes + ':' + this.state.seconds})
+             } else {
+               this.setState({timerText: this.state.minutes + ':' + this.state.seconds})
+             }
+            } else {
+              this.setState({minutes: this.state.minutes + 1, seconds: 0});
+              this.setState({timerText: '0' + this.state.minutes + ':00'});
+           }
+           this.timer = setTimeout(this.updateSomething.bind(this), 1000);
+        }
 
     startTimer() {
         this.timer = setTimeout(this.updateSomething.bind(this), 1000);
     }
 
     render() {
+        console.log(this.state)
         return (
             <View>
                 <TouchableOpacity
@@ -31,7 +50,7 @@ export class Timer extends Component {
                   <Text>Start Timer</Text>
                 </TouchableOpacity>
                 <Text>
-                    {this.state.number}
+                    {this.state.timerText}
                 </Text>
             </View>
             );
