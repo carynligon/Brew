922048dc5483d4e9074d9922996f7e1098c238b6
Caryn Ligon <carynligon@gmail.com>
2017-02-06 23:01:01 -0600

    instructions rendering dynamically based on time

--- src/app/Components/instructions.js
+++ src/app/Components/instructions.js
@@ -3,13 +3,31 @@ import {
     Text,
     View
 } from 'react-native';
+import chemexObj from '../Config/Methods/chemex';
 
 
 export default class Instructions extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {currentStep: null};
+    }
     render() {
-        <View>
-            <Text>
-            </Text>
-        </View>
+        let instruction;
+        if (this.props.time <= chemexObj.steps[0].time.stop) {
+            instruction = chemexObj.steps[0].directions;
+        }
+        else if (this.props.time <= chemexObj.steps[1].time.stop) {
+            instruction = chemexObj.steps[1].directions;
+        }
+        else if (this.props.time <= chemexObj.steps[2].time.stop) {
+            instruction = chemexObj.steps[2].directions;
+        }
+        return (
+            <View>
+                <Text>
+                    {instruction}
+                </Text>
+            </View>
+            );
     }
 }
--- src/app/Config/Methods/chemex.js
+++ src/app/Config/Methods/chemex.js
@@ -2,16 +2,19 @@ export default chemexObj = {
     name: 'chemex',
     coffeeWeight: '41',
     waterWeight: '700',
-    time: '240'
+    time: '240',
     steps: [
         {
             order: 1,
+            time: { start: 0, stop: 30},
             directions: 'Pouring in concentric circles, saturate grounds with 80g of water'
         }, {
             order: 2,
+            time: { start: 30, stop: 240},
             directions: 'Continue, slowly pouring in concentric circles'
         }, {
             order: 3,
+            time: { start: 240, stop: 240},
             directions: 'Stop pouring once you get to 700g of water'
         }
     ]
