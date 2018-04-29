b3169663f98dbf2c182be7be326186dec51171ee
Caryn Ligon <carynligon@gmail.com>
2018-04-29 11:10:04 -0500

    moving src folder

---
+++ tempSrc/src/app/settings.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBfykoOMs_6DxxweE2YSdCuVzL_pdthbI8",
+    authDomain: "brew-dc844.firebaseapp.com",
+    databaseURL: "https://brew-dc844.firebaseio.com",
+    projectId: "brew-dc844",
+    storageBucket: "brew-dc844.appspot.com",
+    messagingSenderId: "513537200169"
+  };
+
